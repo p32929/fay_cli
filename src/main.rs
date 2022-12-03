c3eade0bd4b4431866b334458777c6c4a9ff9b99
p32929 <p32929@yahoo.com>
2022-12-03 23:07:07 +0600

    okay outputs

--- src/main.rs
+++ src/main.rs
@@ -293,9 +293,21 @@ fn run_commands(commands: &CommandData) {
             .stderr(Stdio::piped())
             .spawn();
 
-        let output = spawned.unwrap().wait_with_output().expect("Failed to read stdout");
-        print!("{}", String::from_utf8_lossy(&output.stdout));
-        
+        match spawned {
+            Ok(mut child) => {
+                print!("{}", proc_command.status().unwrap());
+
+                let mut stdin = child.stdin.take().expect("Failed to open stdin");
+                std::thread::spawn(move || {
+                    stdin
+                        .write_all("Hello, world!".as_bytes())
+                        .expect("Failed to write to stdin");
+                });
+                let output = child.wait_with_output().expect("Failed to read stdout");
+                print!("{}", String::from_utf8_lossy(&output.stdout));
+            }
+            Err(error) => eprintln!("{}", error),
+        }
     }
 }
 
